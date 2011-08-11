17e4ea1d166ede848d6643e6151c84c4361c6af4
Helena Moreno Chacon <helenaenred@gmail.com>
2011-08-11 20:38:48 +0200

    Adding fancybox to projects gallery - fixing showing by project

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,81 +10,26 @@
 
 // Application javascript
 
-$(document).ready(function() {
-	/*
-	*   Examples - images
-	*/
-
-	$("a#example1").fancybox();
-
-	$("a#example2").fancybox({
-		'overlayShow'	: false,
-		'transitionIn'	: 'elastic',
-		'transitionOut'	: 'elastic'
-	});
-
-	$("a#example3").fancybox({
-		'transitionIn'	: 'none',
-		'transitionOut'	: 'none'	
-	});
-
-	$("a#example4").fancybox({
-		'opacity'		: true,
-		'overlayShow'	: false,
-		'transitionIn'	: 'elastic',
-		'transitionOut'	: 'none'
-	});
-
-	$("a#example5").fancybox();
-
-	$("a#example6").fancybox({
-		'titlePosition'		: 'outside',
+$(document).ready(function() {	
+	
+	$("a[class=project_gallery]").fancybox({
+		'opacity'			: true,
+		'transitionIn'		: 'elastic',
+		'transitionOut'		: 'elastic',
+		'overlayOpacity'	: 0.9,
 		'overlayColor'		: '#000',
-		'overlayOpacity'	: 0.9
-	});
-
-	$("a#example7").fancybox({
-		'titlePosition'	: 'inside'
-	});
-
-	$("a#example8").fancybox({
-		'titlePosition'	: 'over'
-	});
-
-	$("a[rel=example_group]").fancybox({
-		'transitionIn'		: 'none',
-		'transitionOut'		: 'none',
 		'titlePosition' 	: 'over',
 		'titleFormat'		: function(title, currentArray, currentIndex, currentOpts) {
 			return '<span id="fancybox-title-over">Image ' + (currentIndex + 1) + ' / ' + currentArray.length + (title.length ? ' &nbsp; ' + title : '') + '</span>';
 		}
 	});
+	
 
-	/*
-	*   Examples - various
-	*/
-
-	$("#various1").fancybox({
-		'titlePosition'		: 'inside',
-		'transitionIn'		: 'none',
-		'transitionOut'		: 'none'
+	$("a#my_photo").fancybox({
+		'overlayShow'	: false,
+		'transitionIn'	: 'elastic',
+		'transitionOut'	: 'elastic'
 	});
 
-	$("#various2").fancybox();
 
-	$("#various3").fancybox({
-		'width'				: '75%',
-		'height'			: '75%',
-		'autoScale'			: false,
-		'transitionIn'		: 'none',
-		'transitionOut'		: 'none',
-		'type'				: 'iframe'
-	});
-
-	$("#various4").fancybox({
-		'padding'			: 0,
-		'autoScale'			: false,
-		'transitionIn'		: 'none',
-		'transitionOut'		: 'none'
-	});
 });
