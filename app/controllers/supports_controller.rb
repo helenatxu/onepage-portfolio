54e8b8c65df3e4a09bff5db0e99af57984f196f8
Helena Moreno Chacon <helenaenred@gmail.com>
2011-08-14 20:10:47 +0200

    Fixed validation form - now adding translation

--- app/controllers/supports_controller.rb
+++ app/controllers/supports_controller.rb
@@ -8,15 +8,16 @@ class SupportsController < ApplicationController
   def create
     @support = Support.new(params[:support])
     if @support.save
-      redirect_to root_url, :notice => "Support was successfully sent."
-      #     redirect_to('#contact', :notice => "Support was successfully sent.")
+      #      redirect_to root_url, :notice => "Support was successfully sent."
+      redirect_to '#contact', :notice => "Support was successfully sent."
     else
       if @support.errors.any?
-        
-        flash[:notice] =  @support.errors.full_messages
+        flash[:error_name] = @support.errors[:sender_name][0]
+        flash[:error_email] = @support.errors[:email][0]
+        flash[:error_content] = @support.errors[:content][0]
+        # flash[:notice] =  @support.errors.full_messages
       end
       redirect_to('#contact')
-#      flash[:alert] = "You must fill all fields."
     end
   end 
 
--- app/models/support.rb
+++ app/models/support.rb
@@ -1,25 +1,26 @@
 # app/models/support.rb
 class Support
   include ActiveModel::Validations
- 
+
   validates_presence_of :email, :sender_name, :content
   # to deal with form, you must have an id attribute
   attr_accessor :id, :email, :sender_name, :content
- 
+
+
   def initialize(attributes = {})
     attributes.each do |key, value|
       self.send("#{key}=", value)
     end
     @attributes = attributes
   end
- 
+
   def read_attribute_for_validation(key)
     @attributes[key]
   end
- 
+
   def to_key
   end
- 
+
   def save
     if self.valid?
       Notifier.support_notification(self).deliver
@@ -27,4 +28,15 @@ class Support
     end
     return false
   end
+  
+  HUMANIZED_ATTRIBUTES = {
+      :email => "E-mail address",
+      :sender_name => "Name",
+      :content => "Message"
+    }
+
+    def self.human_attribute_name(attr, options={})
+      HUMANIZED_ATTRIBUTES[attr.to_sym] || super
+    end
+
 end
