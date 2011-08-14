b98d5008325f684f92907f2239f011de33d047b5
Helena Moreno Chacon <helenaenred@gmail.com>
2011-08-14 22:09:31 +0200

    Fixed translation and redirect (more or less)

--- app/controllers/supports_controller.rb
+++ app/controllers/supports_controller.rb
@@ -10,25 +10,32 @@ class SupportsController < ApplicationController
       locale = 'es'
     else
       locale = 'en'
-      end
+    end
     @support = Support.new(params[:support])
     if @support.save
-      #      redirect_to root_url, :notice => "Support was successfully sent."
+      # redirect_to root_url, :notice => "Support was successfully sent."
       # redirect_to '/', :notice_sent => t(:sent)
-      redirect_to '#contact'
-      flash[:notice_sent] = t(:sent)
+      if locale == 'es'
+        redirect_to(root_path+'#contact', :locale => "es") 
+        flash[:notice_sent] = t(:sent)
+      else 
+        redirect_to(root_path+'#contact', :locale => "en")
+        flash[:notice_sent] = t(:sent) 
+      end 
+
     else
       if @support.errors.any?
         flash[:error_name] = @support.errors[:sender_name][0]
         flash[:error_email] = @support.errors[:email][0]
         flash[:error_content] = @support.errors[:content][0]
-        # flash[:notice] =  @support.errors.full_messages
       end
-         if locale == 'es'
-        	  redirect_to('#contact', :locale => 'es')
-        	 else 
-        		redirect_to('#contact', :locale => 'en')
-         end 
+      if locale == 'es'
+        redirect_to(root_path+'#contact', :locale => "es") 
+        # redirect_to '#contact'
+        # locale => 'es'
+      else 
+        redirect_to(root_path+'#contact', :locale => "en") 
+      end 
     end
   end 
 
