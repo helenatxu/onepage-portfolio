dd94c95d44d9d1e05d25f9f0a2eaa45555774325
Helena Moreno Chacon <helenaenred@gmail.com>
2011-08-30 00:13:54 +0200

    Re-fixing translation

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,8 +3,12 @@ class ApplicationController < ActionController::Base
   
   before_filter :set_locale
 
+  def set_locale
+    I18n.locale = extract_locale_from_tld || I18n.default_locale
+  end
+  
   # def set_locale
-  #   I18n.locale = extract_locale_from_tld || I18n.default_locale
+  #   I18n.locale = params[:locale] || I18n.default_locale
   # end
 
   # Get locale from top-level domain or return nil if such locale is not available
@@ -13,15 +17,12 @@ class ApplicationController < ActionController::Base
   #   127.0.0.1 application.it
   #   127.0.0.1 application.pl
   # in your /etc/hosts file to try this out locally
+  
   def extract_locale_from_tld
     parsed_locale = request.host.split('.').last
     I18n.available_locales.include?(parsed_locale.to_sym) ? parsed_locale  : nil
   end
-    
-  
-  def set_locale
-    I18n.locale = params[:locale] || I18n.default_locale
-  end
+
   
   def default_url_options(options={})
     logger.debug "default_url_options is passed options: #{options.inspect}\n"
