8a575d21b8c978edcdb0d962947d679b0292769b
Helena Moreno Chacon <helenaenred@gmail.com>
2011-08-29 17:08:50 +0200

    Changing language selection

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,22 @@ class ApplicationController < ActionController::Base
   
   before_filter :set_locale
 
+  # def set_locale
+  #   I18n.locale = extract_locale_from_tld || I18n.default_locale
+  # end
+
+  # Get locale from top-level domain or return nil if such locale is not available
+  # You have to put something like:
+  #   127.0.0.1 application.com
+  #   127.0.0.1 application.it
+  #   127.0.0.1 application.pl
+  # in your /etc/hosts file to try this out locally
+  def extract_locale_from_tld
+    parsed_locale = request.host.split('.').last
+    I18n.available_locales.include?(parsed_locale.to_sym) ? parsed_locale  : nil
+  end
+  
+  
   def set_locale
     I18n.locale = params[:locale] || I18n.default_locale
   end
