5f39bbbb5208279a07adcb83212923941f18d441
Helena Moreno Chacon <helenaenred@gmail.com>
2011-10-01 18:46:12 +0200

    fixed varius nivo slider adding different ids

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,7 +11,7 @@
 $(document).ready(function() {
 	
 	$(window).load(function() {
-	    $('#slider').nivoSlider({
+	    $('#slider1').nivoSlider({
 	        effect: 'fade', // Specify sets like: 'fold,fade,sliceDown'
 	        // slices: 15, // For slice animations
 	        boxCols: 8, // For box animations
@@ -39,9 +39,100 @@ $(document).ready(function() {
 	        afterLoad: function(){} // Triggers when slider has loaded
 	    });
 	});
+	
+	$(window).load(function() {
+	    $('#slider2').nivoSlider({
+	        effect: 'fade', // Specify sets like: 'fold,fade,sliceDown'
+	        // slices: 15, // For slice animations
+	        boxCols: 8, // For box animations
+	        boxRows: 4, // For box animations
+	        animSpeed: 500, // Slide transition speed
+	        pauseTime: 3000, // How long each slide will show
+	        startSlide: 0, // Set starting Slide (0 index)
+	        directionNav: true, // Next & Prev navigation
+	        directionNavHide: true, // Only show on hover
+	        controlNav: true, // 1,2,3... navigation
+	        controlNavThumbs: true, // Use thumbnails for Control Nav
+	        controlNavThumbsFromRel: false, // Use image rel for thumbs
+	        controlNavThumbsSearch: '.png', // Replace this with...
+	        controlNavThumbsReplace: '-small.png', // ...this in thumb Image src
+	        keyboardNav: true, // Use left & right arrows
+	        pauseOnHover: true, // Stop animation while hovering
+	        manualAdvance: false, // Force manual transitions
+	        captionOpacity: 0.8, // Universal caption opacity
+	        prevText: 'Prev', // Prev directionNav text
+	        nextText: 'Next', // Next directionNav text
+	        beforeChange: function(){}, // Triggers before a slide transition
+	        afterChange: function(){}, // Triggers after a slide transition
+	        slideshowEnd: function(){}, // Triggers after all slides have been shown
+	        lastSlide: function(){}, // Triggers when last slide is shown
+	        afterLoad: function(){} // Triggers when slider has loaded
+	    });
+	});
+	
+	$(window).load(function() {
+	    $('#slider3').nivoSlider({
+	        effect: 'fade', // Specify sets like: 'fold,fade,sliceDown'
+	        // slices: 15, // For slice animations
+	        boxCols: 8, // For box animations
+	        boxRows: 4, // For box animations
+	        animSpeed: 500, // Slide transition speed
+	        pauseTime: 3000, // How long each slide will show
+	        startSlide: 0, // Set starting Slide (0 index)
+	        directionNav: true, // Next & Prev navigation
+	        directionNavHide: true, // Only show on hover
+	        controlNav: true, // 1,2,3... navigation
+	        controlNavThumbs: true, // Use thumbnails for Control Nav
+	        controlNavThumbsFromRel: false, // Use image rel for thumbs
+	        controlNavThumbsSearch: '.png', // Replace this with...
+	        controlNavThumbsReplace: '-small.png', // ...this in thumb Image src
+	        keyboardNav: true, // Use left & right arrows
+	        pauseOnHover: true, // Stop animation while hovering
+	        manualAdvance: false, // Force manual transitions
+	        captionOpacity: 0.8, // Universal caption opacity
+	        prevText: 'Prev', // Prev directionNav text
+	        nextText: 'Next', // Next directionNav text
+	        beforeChange: function(){}, // Triggers before a slide transition
+	        afterChange: function(){}, // Triggers after a slide transition
+	        slideshowEnd: function(){}, // Triggers after all slides have been shown
+	        lastSlide: function(){}, // Triggers when last slide is shown
+	        afterLoad: function(){} // Triggers when slider has loaded
+	    });
+	});
+	
+	$(window).load(function() {
+	    $('#slider4').nivoSlider({
+	        effect: 'fade', // Specify sets like: 'fold,fade,sliceDown'
+	        // slices: 15, // For slice animations
+	        boxCols: 8, // For box animations
+	        boxRows: 4, // For box animations
+	        animSpeed: 500, // Slide transition speed
+	        pauseTime: 3000, // How long each slide will show
+	        startSlide: 0, // Set starting Slide (0 index)
+	        directionNav: true, // Next & Prev navigation
+	        directionNavHide: true, // Only show on hover
+	        controlNav: true, // 1,2,3... navigation
+	        controlNavThumbs: true, // Use thumbnails for Control Nav
+	        controlNavThumbsFromRel: false, // Use image rel for thumbs
+	        controlNavThumbsSearch: '.png', // Replace this with...
+	        controlNavThumbsReplace: '-small.png', // ...this in thumb Image src
+	        keyboardNav: true, // Use left & right arrows
+	        pauseOnHover: true, // Stop animation while hovering
+	        manualAdvance: false, // Force manual transitions
+	        captionOpacity: 0.8, // Universal caption opacity
+	        prevText: 'Prev', // Prev directionNav text
+	        nextText: 'Next', // Next directionNav text
+	        beforeChange: function(){}, // Triggers before a slide transition
+	        afterChange: function(){}, // Triggers after a slide transition
+	        slideshowEnd: function(){}, // Triggers after all slides have been shown
+	        lastSlide: function(){}, // Triggers when last slide is shown
+	        afterLoad: function(){} // Triggers when slider has loaded
+	    });
+	});
+	
 
 
-	$('#slider .nivo-imageLink').live('click',function(event){
+	$('#slider1 .nivo-imageLink').live('click',function(event){
 		event.preventDefault();
 		$.fancybox({
 			'href':$(this).attr('href'), 
