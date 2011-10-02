5f79d1af58dbd1c7f29c6ef301cec4476d6bcc9a
Helena Moreno Chacon <helenaenred@gmail.com>
2011-10-02 14:50:56 +0200

    Thumbnails fixed adding new ids to css

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,8 +12,8 @@ $(document).ready(function() {
 	
 	$(window).load(function() {
 	    $('#slider1').nivoSlider({
-	        effect: 'fade', // Specify sets like: 'fold,fade,sliceDown'
-	        // slices: 15, // For slice animations
+	        effect: 'fold', // Specify sets like: 'fold,fade,sliceDown'
+	        slices: 1, // For slice animations
 	        boxCols: 8, // For box animations
 	        boxRows: 4, // For box animations
 	        animSpeed: 500, // Slide transition speed
