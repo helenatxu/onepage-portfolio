8c38f0b4db356bf3a4e87c346797fc1fe045e6e8
Helena Moreno Chacon <helenaenred@gmail.com>
2011-10-08 21:12:18 +0200

    Completed subtitution of roundabout to nivo slider & lightbox

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -9,130 +9,37 @@
 //= require_tree .
 
 $(document).ready(function() {
-	
-	$(window).load(function() {
-	    $('#slider1').nivoSlider({
-	        effect: 'fade', // Specify sets like: 'fold,fade,sliceDown'
-	        slices: 1, // For slice animations
-	        boxCols: 8, // For box animations
-	        boxRows: 4, // For box animations
-	        animSpeed: 500, // Slide transition speed
-	        pauseTime: 3000, // How long each slide will show
-	        startSlide: 0, // Set starting Slide (0 index)
-	        directionNav: true, // Next & Prev navigation
-	        directionNavHide: true, // Only show on hover
-	        controlNav: true, // 1,2,3... navigation
-	        controlNavThumbs: true, // Use thumbnails for Control Nav
-	        controlNavThumbsFromRel: false, // Use image rel for thumbs
-	        controlNavThumbsSearch: '.png', // Replace this with...
-	        controlNavThumbsReplace: '-small.png', // ...this in thumb Image src
-	        keyboardNav: true, // Use left & right arrows
-	        pauseOnHover: true, // Stop animation while hovering
-	        manualAdvance: false, // Force manual transitions
-	        captionOpacity: 0.84, // Universal caption opacity
-	        prevText: 'Prev', // Prev directionNav text
-	        nextText: 'Next', // Next directionNav text
-	        beforeChange: function(){}, // Triggers before a slide transition
-	        afterChange: function(){}, // Triggers after a slide transition
-	        slideshowEnd: function(){}, // Triggers after all slides have been shown
-	        lastSlide: function(){}, // Triggers when last slide is shown
-	        afterLoad: function(){} // Triggers when slider has loaded
-	    });
+
+	$('#slider1, #slider2, #slider3, #slider4').nivoSlider({
+		effect: 'fade', // Specify sets like: 'fold,fade,sliceDown'
+		slices: 1, // For slice animations
+		boxCols: 8, // For box animations
+		boxRows: 4, // For box animations
+		animSpeed: 500, // Slide transition speed
+		pauseTime: 3000, // How long each slide will show
+		startSlide: 0, // Set starting Slide (0 index)
+		directionNav: true, // Next & Prev navigation
+		directionNavHide: true, // Only show on hover
+		controlNav: true, // 1,2,3... navigation
+		controlNavThumbs: true, // Use thumbnails for Control Nav
+		controlNavThumbsFromRel: false, // Use image rel for thumbs
+		controlNavThumbsSearch: '.png', // Replace this with...
+		controlNavThumbsReplace: '-small.png', // ...this in thumb Image src
+		keyboardNav: true, // Use left & right arrows
+		pauseOnHover: true, // Stop animation while hovering
+		manualAdvance: false, // Force manual transitions
+		captionOpacity: 0.84, // Universal caption opacity
+		prevText: 'Prev', // Prev directionNav text
+		nextText: 'Next', // Next directionNav text
+		beforeChange: function(){}, // Triggers before a slide transition
+		afterChange: function(){}, // Triggers after a slide transition
+		slideshowEnd: function(){}, // Triggers after all slides have been shown
+		lastSlide: function(){}, // Triggers when last slide is shown
+		afterLoad: function(){} // Triggers when slider has loaded
 	});
-	
-	$(window).load(function() {
-	    $('#slider2').nivoSlider({
-	        effect: 'fade', // Specify sets like: 'fold,fade,sliceDown'
-	        // slices: 15, // For slice animations
-	        boxCols: 8, // For box animations
-	        boxRows: 4, // For box animations
-	        animSpeed: 500, // Slide transition speed
-	        pauseTime: 3000, // How long each slide will show
-	        startSlide: 0, // Set starting Slide (0 index)
-	        directionNav: true, // Next & Prev navigation
-	        directionNavHide: true, // Only show on hover
-	        controlNav: true, // 1,2,3... navigation
-	        controlNavThumbs: true, // Use thumbnails for Control Nav
-	        controlNavThumbsFromRel: false, // Use image rel for thumbs
-	        controlNavThumbsSearch: '.png', // Replace this with...
-	        controlNavThumbsReplace: '-small.png', // ...this in thumb Image src
-	        keyboardNav: true, // Use left & right arrows
-	        pauseOnHover: true, // Stop animation while hovering
-	        manualAdvance: false, // Force manual transitions
-	        captionOpacity: 0.8, // Universal caption opacity
-	        prevText: 'Prev', // Prev directionNav text
-	        nextText: 'Next', // Next directionNav text
-	        beforeChange: function(){}, // Triggers before a slide transition
-	        afterChange: function(){}, // Triggers after a slide transition
-	        slideshowEnd: function(){}, // Triggers after all slides have been shown
-	        lastSlide: function(){}, // Triggers when last slide is shown
-	        afterLoad: function(){} // Triggers when slider has loaded
-	    });
-	});
-	
-	$(window).load(function() {
-	    $('#slider3').nivoSlider({
-	        effect: 'fade', // Specify sets like: 'fold,fade,sliceDown'
-	        // slices: 15, // For slice animations
-	        boxCols: 8, // For box animations
-	        boxRows: 4, // For box animations
-	        animSpeed: 500, // Slide transition speed
-	        pauseTime: 3000, // How long each slide will show
-	        startSlide: 0, // Set starting Slide (0 index)
-	        directionNav: true, // Next & Prev navigation
-	        directionNavHide: true, // Only show on hover
-	        controlNav: true, // 1,2,3... navigation
-	        controlNavThumbs: true, // Use thumbnails for Control Nav
-	        controlNavThumbsFromRel: false, // Use image rel for thumbs
-	        controlNavThumbsSearch: '.png', // Replace this with...
-	        controlNavThumbsReplace: '-small.png', // ...this in thumb Image src
-	        keyboardNav: true, // Use left & right arrows
-	        pauseOnHover: true, // Stop animation while hovering
-	        manualAdvance: false, // Force manual transitions
-	        captionOpacity: 0.8, // Universal caption opacity
-	        prevText: 'Prev', // Prev directionNav text
-	        nextText: 'Next', // Next directionNav text
-	        beforeChange: function(){}, // Triggers before a slide transition
-	        afterChange: function(){}, // Triggers after a slide transition
-	        slideshowEnd: function(){}, // Triggers after all slides have been shown
-	        lastSlide: function(){}, // Triggers when last slide is shown
-	        afterLoad: function(){} // Triggers when slider has loaded
-	    });
-	});
-	
-	$(window).load(function() {
-	    $('#slider4').nivoSlider({
-	        effect: 'fade', // Specify sets like: 'fold,fade,sliceDown'
-	        // slices: 15, // For slice animations
-	        boxCols: 8, // For box animations
-	        boxRows: 4, // For box animations
-	        animSpeed: 500, // Slide transition speed
-	        pauseTime: 3000, // How long each slide will show
-	        startSlide: 0, // Set starting Slide (0 index)
-	        directionNav: true, // Next & Prev navigation
-	        directionNavHide: true, // Only show on hover
-	        controlNav: true, // 1,2,3... navigation
-	        controlNavThumbs: true, // Use thumbnails for Control Nav
-	        controlNavThumbsFromRel: false, // Use image rel for thumbs
-	        controlNavThumbsSearch: '.png', // Replace this with...
-	        controlNavThumbsReplace: '-small.png', // ...this in thumb Image src
-	        keyboardNav: true, // Use left & right arrows
-	        pauseOnHover: true, // Stop animation while hovering
-	        manualAdvance: false, // Force manual transitions
-	        captionOpacity: 0.8, // Universal caption opacity
-	        prevText: 'Prev', // Prev directionNav text
-	        nextText: 'Next', // Next directionNav text
-	        beforeChange: function(){}, // Triggers before a slide transition
-	        afterChange: function(){}, // Triggers after a slide transition
-	        slideshowEnd: function(){}, // Triggers after all slides have been shown
-	        lastSlide: function(){}, // Triggers when last slide is shown
-	        afterLoad: function(){} // Triggers when slider has loaded
-	    });
-	});
-	
 
 
-	$('#slider1 .nivo-imageLink').live('click',function(event){
+	$('#slider1 .nivo-imageLink, #slider2 .nivo-imageLink,  #slider3 .nivo-imageLink,  #slider4 .nivo-imageLink').live('click',function(event){
 		event.preventDefault();
 		$.fancybox({
 			'href':$(this).attr('href'), 
@@ -141,44 +48,23 @@ $(document).ready(function() {
 		});
 	});
 
-	// $(function() {
-	// 	$('ul.myRoundabout').roundabout({
-	// 		shape:'lazySusan', 
-	// 		clickToFocus: true, 
-	// 		minScale:0.2, 
-	// 		easing: 'easeOutInCirc', 
-	// 		duration: 800, 
-	// 		tilt: -1.5,
-	// 		btnNext: '.button_prev',
-	// 		btnPrev: '.button_next'
-	// 	});
-	// });
-	// 
-	// $('ul.myRoundabout .roundabout-in-focus a').live('click',function(event){
-	// 	event.preventDefault();
-	// 	$.fancybox({
-	// 		'href':$(this).attr('href'), 
-	// 		opacity: 0.9, 
-	// 		overlayColor: '#000'
-	// 	});
-	// });
-	
-	// $(document).ready(function() {
-	// 	$('.roundabout-in-focus a img').hover(function() {
-	// 		grayscale( $(this) );
-	// 	}, function() {
-	// 		grayscale.reset( $(this) );
-	// 	});
-	// 	grayscale.prepare( $('.roundabout-in-focus a img') );
-	// });
-	
-	// $('ul.myRoundabout li').hover(
-	// function () {
-	// $('img', this).fadeTo('normal', 0.5);
-	// },
-	// function () {
-	// $('img', this).fadeTo('normal', 1);
-	// }
-	// );
 
-	});
+	// $(document).ready(function() {
+		// 	$('.roundabout-in-focus a img').hover(function() {
+			// 		grayscale( $(this) );
+			// 	}, function() {
+				// 		grayscale.reset( $(this) );
+				// 	});
+				// 	grayscale.prepare( $('.roundabout-in-focus a img') );
+				// });
+
+				// $('ul.myRoundabout li').hover(
+					// function () {
+						// $('img', this).fadeTo('normal', 0.5);
+						// },
+						// function () {
+							// $('img', this).fadeTo('normal', 1);
+							// }
+							// );
+
+						});
